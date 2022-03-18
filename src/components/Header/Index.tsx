84ca7fc9b2affcfab053d206c57fe18a54d76997
Lucas Stofella <lucas.stofella2@havan.com.br>
2022-03-18 18:00:08 -0300

    Arrumando a rota entre as paginas e finalizando a chamada dos posts

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,14 +1,26 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router';
+
 import { SignInBuntton } from '../SignInButton';
+import { ActiveLink } from './ActiveLink';
+
 import styles from './styles.module.scss'
 
 export function Header() {
+
+    const { asPath } = useRouter()
+
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active} href="">Home</a>
-                    <a href="">Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 
                 <SignInBuntton/>
